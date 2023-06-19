d581befe43ef892d90040173c4e67057b6c852c5
malithw1987465 <maliith.20223218@iit.ac.lk>
2023-06-19 13:36:54 +0530

    Deleted ClientForm.java

--- src/main/java/Controllers/LoginForm.java
+++ src/main/java/Controllers/LoginForm.java
@@ -5,34 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 
 
 import java.io.IOException;
 
 
 public class LoginForm extends Application {
-    public Button btnLogin;
-    public TextField txtUserName;
-    public AnchorPane dashboardPane;
-
-    public void btnLoginOnAction(ActionEvent event) throws IOException {
-        String name=txtUserName.getText();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/ServerForm.fxml"))));
-        stage.setTitle(name+" Room");
-        stage.centerOnScreen();
-        stage.show();
-    }
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/View/LoginForm.fxml"));
--- src/main/java/Controllers/LoginFormController.java
+++ src/main/java/Controllers/LoginFormController.java
@@ -1,4 +1,26 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public Button btnLogin;
+    public TextField txtUserName;
+    public AnchorPane dashboardPane;
+    public void btnLoginOnAction(ActionEvent event) throws IOException {
+        String name=txtUserName.getText();
+        Stage stage = (Stage) dashboardPane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/ServerForm.fxml"))));
+        stage.setTitle(name+"Chat  Room");
+        stage.centerOnScreen();
+        stage.show();
+
+    }
 }
