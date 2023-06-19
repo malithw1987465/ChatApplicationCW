c8c981fdb309a4a5400171ea7d6420d5ce11d8e1
malithw1987465 <maliith.20223218@iit.ac.lk>
2023-06-19 13:21:26 +0530

    Added LoginFormController

--- src/main/java/Controllers/LoginForm.java
+++ src/main/java/Controllers/LoginForm.java
@@ -1,29 +1,44 @@
 package Controllers;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
-public class LoginForm {
+
+public class LoginForm extends Application {
     public Button btnLogin;
     public TextField txtUserName;
     public AnchorPane dashboardPane;
-    public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
-        if(txtUserName.getText().equals("Malith")){
-            Stage stage = (Stage) dashboardPane.getScene().getWindow();
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/ClientForm.fxml"))));
-            stage.setTitle("Login");
-            stage.centerOnScreen();
-            stage.show();
-        }else{
-            new Alert(Alert.AlertType.ERROR, "Not a existing user!").show();
-        }
+
+    public void btnLoginOnAction(ActionEvent event) throws IOException {
+        String name=txtUserName.getText();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/ServerForm.fxml"))));
+        stage.setTitle(name+" Room");
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/LoginForm.fxml"));
+        primaryStage.setTitle("Login Portal");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
     }
 }
