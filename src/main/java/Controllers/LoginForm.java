0d920ebfe0a1674018cd34665c75b2080fb8aabb
malithw1987465 <maliith.20223218@iit.ac.lk>
2023-06-19 12:00:52 +0530

    Added icons

--- src/main/java/Controllers/LoginForm.java
+++ src/main/java/Controllers/LoginForm.java
@@ -4,19 +4,22 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginForm {
     public Button btnLogin;
     public TextField txtUserName;
-    public TextField txtPassword;
     public AnchorPane dashboardPane;
-    public void btnLoginOnAction(ActionEvent actionEvent) {
-        if(txtUserName.getText().equals("Malith") && txtPassword.getText().equals("1234")){
+    public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+        if(txtUserName.getText().equals("Malith")){
             Stage stage = (Stage) dashboardPane.getScene().getWindow();
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/LoginForm.fxml"))));
-            stage.setTitle("Item Form");
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/ClientForm.fxml"))));
+            stage.setTitle("Login");
             stage.centerOnScreen();
             stage.show();
         }else{
